c1a97f8251196c67f4c0f9232c38d921688158fc
abhiagarwal18 <itsagarwal18@gmail.com>
2019-07-07 13:57:51 +0530

    initial commit

--- src/myFirstComp.js
+++ src/myFirstComp.js
@@ -1,3 +1,37 @@
 import React, {Component, Fragment } from "react";
 import Button from "./components/button.js"
 import Display from "./components/display.js"
+const buttons= ["9","8","7","6","5","4","3","2","1","0","+","-","*","/","="]
+
+class MyFirstComp extends Component{
+    constructor (props){
+        super(props);
+        this.state={
+            displayValue:""
+        }
+
+    }
+    
+
+
+    render(){
+        return(
+            <Fragment>
+                <h1>
+                    Calculator
+                </h1>
+                <Display value={this.state.displayValue} />
+                {
+                    buttons.map((data,i)=>{
+                        return(
+                            <Button handleClick= {this.changeDisplay} key={i} label={data}/>
+
+                        )
+                    }
+                    )
+                }
+            </Fragment>
+        )
+    }
+}
+export default MyFirstComp
